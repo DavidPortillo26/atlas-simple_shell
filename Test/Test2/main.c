3453c9b9dc485a815aa9a02d45ee65a91c2122c2
= <=>
2024-03-26 11:52:42 -0500

    commiting the main file

---
+++ Test/Test2/main.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * sshell - main function
+ * @:
+ * Return: Always 0
+*/
+
+int sshell(void)
+	{
+
+	}
