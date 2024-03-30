7fe36d823ac2c8fb8d7ce1522676785eec08ef17
Trey Turner <trey.turner1857@gmail.com>
2024-03-29 23:51:03 -0500

    removed the read function to store in its own file

--- Test/Test2/cmd-line.c
+++ Test/Test2/cmd-line.c
@@ -4,38 +4,6 @@
 #include <string.h>
 #include <unistd.h>
 /**
- * main - reads the user input
- * Return: success
-*/
-int main(void)
-{
-	char *cmd, /*command*/ *token = NULL; /*token*/
-	char *delimiter = " \n";
-	char **array;
-	int k = 0; /*index*/
-	int size_limit = 1024; /*size limit for array*/
-	size_t n = 0; /*size of command*/
-
-	while (1) /*infinite loop*/
-	{
-		write(1, "SimpleShell$ ", 13);
-		getline(&cmd, &n, stdin); /*reads the input*/
-		token = strtok(cmd, delimiter);
-		array = malloc(sizeof(char *) * size_limit) /*array space allocation*/
-
-		while (token != NULL) /*creates an array of tokens*/
-		{
-			array[k] = token;
-			token = strtok(cmd, delimiter)
-		}
-
-		if (token != NULL) /*if token doesnt = NULL send into the func cmdline*/
-		{
-			cmdline(token);
-		}
-	}
-}
-/**
  * cmdline - handles the command line args
  * @prompt: argument that is passed in
  * Return: Success
