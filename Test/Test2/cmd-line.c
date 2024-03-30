981af4f900f9ad13fb323302e8618de96904c355
= <=>
2024-03-29 23:10:27 -0500

    added the main function to read the user input (unfinished)

--- Test/Test2/cmd-line.c
+++ Test/Test2/cmd-line.c
@@ -4,15 +4,47 @@
 #include <string.h>
 #include <unistd.h>
 /**
+ * main - reads the user input
+ * Return: success
+*/
+int main(void)
+{
+	char *cmd, /*command*/ *token = NULL; /*token*/
+	char *delimiter = " \n";
+	char **array;
+	int k = 0; /*index*/
+	size_t n = 0; /*size of command*/
+
+	while (1) /*infinite loop*/
+	{
+		write(1, "SimpleShell$ ", 13);
+		getline(&cmd, &n, stdin); /*reads the input*/
+		token = strtok(cmd, delimiter);
+
+		while (token != NULL) /*creates an array of tokens*/
+		{
+			array[k] = token;
+			token = strtok(cmd, delimiter)
+		}
+
+		if (token != NULL) /*if token doesnt = NULL send into the func cmdline*/
+		{
+			cmdline(token);
+		}
+	}
+}
+/**
  * cmdline - handles the command line args
  * @prompt: argument that is passed in
- * Return: 
+ * Return: Success
 */
 void cmdline(char *prompt)
 {
 	int argc; /*arg count*/
 	char *argv; /*arg list*/
 	char modprompt; /*contains a modified prompt*/
+	int background; /*used to wait for foreground process to end*/
+	pid_t childpid = fork();
 
 	unsigned int jid = 0; /*job id*/
 
@@ -22,4 +54,9 @@ void cmdline(char *prompt)
 		return;
 
 	jid++ /*assigns a job id*/
+
+	if (childpid == 0) /*child process*/
+	{
+
+	}
 }
