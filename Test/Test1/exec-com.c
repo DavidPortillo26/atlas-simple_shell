b97ae8664c1712456257bb31142f802764747e37
DavidPortillo26 <anicetod03@gmail.com>
2024-04-01 23:15:13 +0000

    fork process and failure child process and failure

--- Test/Test1/exec-com.c
+++ Test/Test1/exec-com.c
@@ -1,17 +1,32 @@
 #include "main.h"
 
-// Function to handle termination of child processes
+/* Function to handle termination of child processes */
 void sigchildHandler(int signal) {
-    // Handle termination of child process here
+    /* Handle termination of child process here */
 }
 
-// Function to execute system commands
+/* Function to execute system commands */
 void execCommand() {
     int status;
     pid_t pid, wpid;
 
-    // Check if the command is to be executed in the background
+    /* Check if the command is to be executed in the background */
     if (strcmp(args[no_args - 1], "&") == 0) {
         is_bg = 1;
         args[no_args - 1] = NULL;
     }
+ /* Fork a new process */
+    pid = fork();
+    if (pid == -1) { /* Error handling for fork failure */
+        fprintf(stderr, "Fork command has failed: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    } 
+    else if (pid == 0) {  /* Child process */
+        /* Reset signal handler to default behavior */
+        signal(SIGCHLD, SIG_DFL);
+
+        /* Execute the command in the child process */
+        if (execvp(args[0], args) < 0) {
+            fprintf(stderr, "Command could not be executed: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
