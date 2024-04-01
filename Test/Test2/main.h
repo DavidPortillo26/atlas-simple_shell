2c15917504ba715efe5305fac69f4e7ed5ada018
Trey Turner <trey.turner1857@gmail.com>
2024-04-01 14:50:54 -0500

    combined cmdline and userinput and added 2 includes to the main.h file

--- Test/Test2/main.h
+++ Test/Test2/main.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 
 #endif
--- Test/Test2/user-input.c
+++ Test/Test2/user-input.c
@@ -8,11 +8,12 @@ int main(void)
 	char *mem = NULL; /*data from standard input*/
 	size_t size = 0; /*size of mem from the standard input*/
 	ssize_t data;
-	pid_t processid;
+	pid_t childpid = fork(); /*creates a c_pid to execute a command*/
 	int status;
-	bool ispiped = false;
+	bool ispiped = false; /*used to check if argument is piped*/
+	struct stat statbuf;
 
-	while (1) /*infinite loop*/
+	while (1 && !ispiped) /*infinite loop but if piped then only run once */
 	{
 		if (isatty(STDIN_FILENO) == 0)
 			ispiped = true;
@@ -22,5 +23,29 @@ int main(void)
 		/*prints the prompt onto the terminal*/
 
 		data = getline(&mem, &size, stdin); /*reads the data from stdin*/
+
+		if (data == -1)
+		{
+			perror("Error getline()");
+			free(mem); /*free memory if it fails*/
+			exit(EXIT_FAILURE);
+		}
+
+		if (builtin(char *s)) /*if built-in, execute then return*/
+			return;
+
+		if (childpid == -1) /*checks if child process failed*/
+		{
+			perror("Error fork()");
+			exit(EXIT_FAILURE);
+		}
+
+		if (childpid == 0)
+		{
+			exec
+		}
 	}
+
+	free(mem); /*once func is finished free the memory*/
+	return (0); /*return 0*/
 }
