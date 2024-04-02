dd14cd8320abe05b1b13fb03d7340941ef57e3f3
DavidPortillo26 <anicetod03@gmail.com>
2024-04-02 02:35:19 +0000

    function for ls -a

--- Test/Test1/ls.c
+++ Test/Test1/ls.c
@@ -20,3 +20,20 @@ void ls_command()
     }
     free(filelist); /* Frees memory allocated for the array of directory entries */
 }
+
+// Function to perform ls -a
+void lsa_command()
+{
+    struct dirent **filelist; // Pointer to an array of directory entries
+    int n; // Number of directory entries
+    int i; // Loop counter
+
+    // Scans the current directory and stores directory entries in filelist
+    n = scandir(".", &filelist, NULL, alphasort);
+    for (i = 0; i < n; i++)
+    {
+        printf("%s\n", filelist[i]->d_name); // Prints the name of the file/directory
+        free(filelist[i]); // Frees memory allocated for each directory entry
+    }
+    free(filelist); // Frees memory allocated for the array of directory entries
+}
