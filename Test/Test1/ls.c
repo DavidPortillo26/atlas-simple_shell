0a228020379e61bdf5bdb327f93e51fe643477c9
DavidPortillo26 <anicetod03@gmail.com>
2024-04-02 02:07:36 +0000

    start of ls command

---
+++ Test/Test1/ls.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/* Function to perform ls */
+void ls_command()
+{
+    struct dirent **filelist; /* Pointer to an array of directory entries */
+    int n; /* Number of directory entries */
+    int i; /* Loop counter */
+
+    /* Scans the current directory and stores directory entries in filelist */
+    n = scandir(".", &filelist, NULL, alphasort);
+    for (i = 0; i < n; i++)
+    {
+        /* Filters out hidden files (files starting with dot) */
+        if ((filelist[i]->d_name)[0] != '.')
+        {
+            printf("%s\n", filelist[i]->d_name); /* Prints the name of the file/directory*/
+        }
+        free(filelist[i]); /* Frees memory allocated for each directory entry */
+    }
+    free(filelist); /* Frees memory allocated for the array of directory entries */
+}
