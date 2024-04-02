8ef596376200751c32215de98db0b42a2b63fa45
DavidPortillo26 <anicetod03@gmail.com>
2024-04-02 01:43:44 +0000

    sigchld, loop, termination check, thumbs up msg

--- Test/Test1/sig-handlers.c
+++ Test/Test1/sig-handlers.c
@@ -9,3 +9,22 @@ void sigintHandler(int sig_num)
     /* Flush stdout to ensure any buffered output is written to the console */
     fflush(stdout);
 }
+
+/* Signal handler for SIGCHLD (child process termination) signal */
+void sigchildHandler(int sig_num)
+{
+    pid_t cpid; /* Variable to store child process ID */
+    int status; /* Variable to store child process status */
+
+    /* Loop until no more terminated child processes are waiting to be reaped */
+    while ((cpid = waitpid(-1, &status, WNOHANG)) > 0)
+    {
+        /* Check if the child process terminated normally */
+        if (WIFEXITED(status))
+        {
+            /* Print a message indicating that the child process exited normally */
+            fprintf(stderr, "\n%s with pid %d exited normally\n", args[0], cpid);
+        }
+    }
+    return;
+}
