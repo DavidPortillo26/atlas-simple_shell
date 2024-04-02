4756f8e9caa4eec27e728b3debadd92a6cbb2703
DavidPortillo26 <anicetod03@gmail.com>
2024-04-02 03:37:26 +0000

    ls -a -l

--- Test/Test1/ls.c
+++ Test/Test1/ls.c
@@ -21,56 +21,69 @@ void ls_command()
     free(filelist); /* Frees memory allocated for the array of directory entries */
 }
 
-// Function to perform ls -a
+/* Function to perform ls -a */
 void lsa_command()
 {
-    struct dirent **filelist; // Pointer to an array of directory entries
-    int n; // Number of directory entries
-    int i; // Loop counter
+    struct dirent **filelist; /* Pointer to an array of directory entries */
+    int n; /* Number of directory entries */
+    int i; /* Loop counter */
 
-    // Scans the current directory and stores directory entries in filelist
+    /* Scans the current directory and stores directory entries in filelist */
     n = scandir(".", &filelist, NULL, alphasort);
     for (i = 0; i < n; i++)
     {
-        printf("%s\n", filelist[i]->d_name); // Prints the name of the file/directory
-        free(filelist[i]); // Frees memory allocated for each directory entry
+        printf("%s\n", filelist[i]->d_name); /* Prints the name of the file/directory */
+        free(filelist[i]); /* Frees memory allocated for each directory entry */
     }
-    free(filelist); // Frees memory allocated for the array of directory entries
+    free(filelist); /* Frees memory allocated for the array of directory entries */
 }
-// Function to perform ls -l
+/* Function to perform ls -l */
 void lsl_command()
 {
-    DIR *dir; // Pointer to a directory stream
-    struct dirent *file; // Pointer to a directory entry
-    struct stat sbuf; // Structure to store file/directory attributes
-    char buf[128]; // Buffer for username and groupname
-    struct passwd pwent, *pwentp; // Structure to store user information
-    struct group grp, *grpt; // Structure to store group information
-    char datestring[256]; // Buffer for formatted date and time
-    struct tm time; // Structure to store broken-down time
-    char perms[11]; // Buffer for file permissions
-    char *path = NULL; // Buffer for the current working directory
+    DIR *dir; /* Pointer to a directory stream */
+    struct dirent *file; /* Pointer to a directory entry */
+    struct stat sbuf; /* Structure to store file/directory attributes */
+    char buf[128]; /* Buffer for username and groupname */
+    struct passwd pwent, *pwentp; /* Structure to store user information */
+    struct group grp, *grpt; /* Structure to store group information */
+    char datestring[256]; /* Buffer for formatted date and time */
+    struct tm time; /* Structure to store broken-down time */
+    char perms[11]; /* Buffer for file permissions */
+    char *path = NULL; /* Buffer for the current working directory */
 
-// Get the current working directory
+/* Get the current working directory */
     getcwd(path, sizeof(path));
 
-    // Open the current directory
+    /* Open the current directory */
     dir = opendir(path);
 
-    // Iterate through each file/directory in the directory
+    /* Iterate through each file/directory in the directory */
     while (file = readdir(dir))
     {
-        // Exclude hidden files (files starting with dot)
+        /* Exclude hidden files (files starting with dot) */
         if ((file->d_name)[0] != '.')
         {
-            // Get file/directory attributes
+            /* Get file/directory attributes */
             stat(file->d_name, &sbuf);
 
-            // Determine file permissions
-            // Populate perms array accordingly
-            // Note: Permissions may vary based on file type (regular file, directory, etc.)
-            // and user/group permissions (read, write, execute)
-            // Print permissions, number of links, username, groupname, file size, last modified date/time, and filename
+            /* Determine file permissions */
+            /* Populate perms array accordingly */
+            /* Note: Permissions may vary based on file type (regular file, directory, etc.) */
+            /* and user/group permissions (read, write, execute) */
+            /* Print permissions, number of links, username, groupname, file size, last modified date/time, and filename */
         }
     }
+}
+/* Function to perform ls -a -l */
+void lsal_command()
+{
+    /* Similar to lsl_command() but includes hidden files/directories */
+}
+
+/* Function to check ls command options and execute corresponding functions */
+void check_ls()
+{
+    /* Check if no ls options specified */
+    /* Execute ls_command() and set execFlag to indicate successful execution */
+    /* Continue similar checks for other ls options and execute corresponding functions */
 }
