cf70ce1134f963ce33c11b31f74dbe4eca4f8987
Trey Turner <trey.turner1857@gmail.com>
2024-04-01 23:02:12 -0500

    implemented the functions in parse-line into user-input. finished fork and path

--- Test/Test2/user-input.c
+++ Test/Test2/user-input.c
@@ -3,15 +3,16 @@
  * main - reads the user input
  * Return: success
 */
-int main(void)
+int main(int argc, char **argv, char **env)
 {
 	char *mem = NULL; /*data from standard input*/
+	char *path; /*stores the result of the getPATH function*/
+	char **args; /*stores the result of the split string function*/
 	size_t size = 0; /*size of mem from the standard input*/
-	ssize_t data;
+	ssize_t data; /*stores data from the getiline function*/
 	pid_t childpid = fork(); /*creates a c_pid to execute a command*/
-	int status;
+	int status; /*status*/
 	bool ispiped = false; /*used to check if argument is piped*/
-	struct stat statbuf;
 
 	while (1 && !ispiped) /*infinite loop but if piped then only run once */
 	{
@@ -31,19 +32,26 @@ int main(void)
 			exit(EXIT_FAILURE);
 		}
 
-		if (builtin(char *s)) /*if built-in, execute then return*/
+		if (builtin(data)) /*if built-in, execute then return*/
 			return;
+											/*spst is short for splitstring*/
+		args = splitstring(mem, " \t\n") /*passes mem and " \t\n" into spst*/
 
-		if (childpid == -1) /*checks if child process failed*/
-		{
-			perror("Error fork()");
-			exit(EXIT_FAILURE);
-		}
+		if (strcmp(args[0], "exit") == 0) /*check if the first arg is exit*/
+			exit(0);
 
-		if (childpid == 0)
+		if (childpid == 0) /*attempt to execute a command if childpid == 0*/
 		{
-			exec
+			path = getPATH(args[0]);
+
+			if (path) /*if full path is returned execute*/
+				execve(path, args, env);
+			else /*if childpid != 0 print command not found*/
+				printf("Command not found\n");
+			exit(0);
 		}
+		else /*if not a childpid then wait for childpid to finish*/
+			wait(&status);
 	}
 
 	free(mem); /*once func is finished free the memory*/
