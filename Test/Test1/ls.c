7ce150fbd82803bb0612534921481e4424a7f035
DavidPortillo26 <anicetod03@gmail.com>
2024-04-02 03:30:55 +0000

    ls -l function and working directories

--- Test/Test1/ls.c
+++ Test/Test1/ls.c
@@ -37,3 +37,40 @@ void lsa_command()
     }
     free(filelist); // Frees memory allocated for the array of directory entries
 }
+// Function to perform ls -l
+void lsl_command()
+{
+    DIR *dir; // Pointer to a directory stream
+    struct dirent *file; // Pointer to a directory entry
+    struct stat sbuf; // Structure to store file/directory attributes
+    char buf[128]; // Buffer for username and groupname
+    struct passwd pwent, *pwentp; // Structure to store user information
+    struct group grp, *grpt; // Structure to store group information
+    char datestring[256]; // Buffer for formatted date and time
+    struct tm time; // Structure to store broken-down time
+    char perms[11]; // Buffer for file permissions
+    char *path = NULL; // Buffer for the current working directory
+
+// Get the current working directory
+    getcwd(path, sizeof(path));
+
+    // Open the current directory
+    dir = opendir(path);
+
+    // Iterate through each file/directory in the directory
+    while (file = readdir(dir))
+    {
+        // Exclude hidden files (files starting with dot)
+        if ((file->d_name)[0] != '.')
+        {
+            // Get file/directory attributes
+            stat(file->d_name, &sbuf);
+
+            // Determine file permissions
+            // Populate perms array accordingly
+            // Note: Permissions may vary based on file type (regular file, directory, etc.)
+            // and user/group permissions (read, write, execute)
+            // Print permissions, number of links, username, groupname, file size, last modified date/time, and filename
+        }
+    }
+}
